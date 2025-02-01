b2d0f16c514dd122cdd0b151e49dbcb3fe5deae9
Pankaj Nanda <21f1002348@ds.study.iitm.ac.in>
2025-02-01 23:36:01 +0530

    Changing Code

--- api/index.py
+++ api/index.py
@@ -15,24 +15,10 @@ app.app_context().push()
 
 with open("q-vercel-python.json", "r") as file:
     data = json.load(file)
-    # print(data)
 
 
-@app.route('/api')
-def Welcome():
-    all_students = data
-    return all_students
 
-@app.route('/a', methods = ['GET'])
-def Students():
-    all_students = data
-    l = []
-    for student in all_students:
-        output = {student['name'] : student['marks']}
-        l.append(output)
-    return ("Welcome" , output)
-
-@app.route('/', methods = ['GET'])
+@app.route('/api', methods = ['GET'])
 def Marks():
     names = request.args.getlist("name")  # Get multiple "name" values from query params
     marks_list = [student["marks"] for student in data if student["name"] in names]
